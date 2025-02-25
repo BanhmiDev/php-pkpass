d3a3047b48553d82dec1c8756d0ecda1c20146d6
Son Nguyen <business@banhmi.dev>
2025-02-25 21:55:44 +0100

    Update PKPass.php

--- src/PKPass.php
+++ src/PKPass.php
@@ -453,29 +453,6 @@ class PKPass
             );
         }
 
-        // Try an alternative route using shell_exec
-        try {
-            $value = @shell_exec(
-                "openssl pkcs12 -in " . escapeshellarg($this->certPath) .
-                " -passin " . escapeshellarg("pass:" . $this->certPass) .
-                " -passout " . escapeshellarg("pass:" . $this->certPass) .
-                " -legacy"
-            );
-            if ($value) {
-                $cert = substr($value, strpos($value, '-----BEGIN CERTIFICATE-----'));
-                $cert = substr($cert, 0, strpos($cert, '-----END CERTIFICATE-----') + 25);
-                $key = substr($value, strpos($value, '-----BEGIN ENCRYPTED PRIVATE KEY-----'));
-                $key = substr($key, 0, strpos($key, '-----END ENCRYPTED PRIVATE KEY-----') + 35);
-                if (strlen($cert) > 0 && strlen($key) > 0) {
-                    $certs['cert'] = $cert;
-                    $certs['pkey'] = $key;
-                    return $certs;
-                }
-            }
-        } catch (\Throwable $e) {
-            // no need to do anything
-        }
-
         throw new PKPassException(
             'Could not read certificate file. This might be related ' .
             'to using an OpenSSL version that has deprecated some older ' .
